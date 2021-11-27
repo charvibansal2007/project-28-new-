556ece79ac94bae6c62761ca6ae4aa416259cea1
charvibansal2007 <76526224+charvibansal2007@users.noreply.github.com>
2021-11-27 15:59:02 +0530

    Add files via upload

---
+++ Launcher.js
@@ -0,0 +1,31 @@
+class Launcher{
+
+    constructor(bodyA , pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.004,
+            length: 100
+        }
+     this.pointB = pointB;
+     this.elastic = Constraint.create(options);
+     World.add(world, this.elastic);
+    }
+
+    attach(body){
+        this.elastic.bodyA = body;
+    }
+
+    fly(){
+        this.elastic.bodyA = null;
+    }
+    display(){
+        if(this.elastic.bodyA){
+            var pointA = this.elastic.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x ,pointA.y , pointB.x , pointB.y);
+        }
+
+    }
+}
